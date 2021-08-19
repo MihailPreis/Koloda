a4f4412425d19d98ffbad1ecc77e50b41ba1ee40
Mihail Preis <mihail.preis@waveaccess.ru>
2021-08-19 21:08:03 +0500

    Down minimal version

--- Package.swift
+++ Package.swift
@@ -6,7 +6,7 @@ import PackageDescription
 let package = Package(
     name: "Koloda",
     platforms: [
-        .iOS(.v12)
+        .iOS(.v11)
     ],
     products: [
         .library(name: "Koloda", targets: ["Koloda"]),
